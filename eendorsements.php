cb72bcad28be494e340190369f1c44e6be9cb246
Justin Dalrymple <wholehogsoftware@gmail.com>
2014-08-17 18:39:43 -0500

    File comments

---
+++ eendorsements.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: eEndorsements.com Wordpress Plugin
+ * Plugin URI: http://eendorsements.com/wordpress
+ * Description: Easily add your eEndorsements.com endorsements, testimonials, and reviews to your Wordpress web site or blog.
+ * Version: 1.0
+ * Author: eEndorsements.com, LLC.
+ * Author URI: http://eendorsements.com
+ * License: MIT
+ */
