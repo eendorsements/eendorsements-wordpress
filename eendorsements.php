f29ae1afacbe8528124e5ac90d1d9fe63cbb3c5d
Justin Dalrymple <wholehogsoftware@gmail.com>
2014-09-05 16:38:14 -0500

    Readme

--- eendorsements.php
+++ eendorsements.php
@@ -1,10 +1,25 @@
 <?php
 /**
  * Plugin Name: eEndorsements.com Wordpress Plugin
- * Plugin URI: http://eendorsements.com/wordpress
+ * Plugin URI: https://github.com/eendorsements/eendorsements-wordpress
  * Description: Easily add your eEndorsements.com endorsements, testimonials, and reviews to your Wordpress web site or blog.
  * Version: 1.0
  * Author: eEndorsements.com, LLC.
  * Author URI: http://eendorsements.com
  * License: GPL2
  */
+ /*  Copyright 2014 eEndorsements.com  (email : admin@eendorsements.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
---
+++ readme.txt
@@ -0,0 +1,19 @@
+=== eEndorsements ===
+Contributors: eEndorsements.com
+Tags: testimonials, reviews, endorsements, references
+Requires at least: 2.8
+Tested up to: 4.0
+
+Easily add your eEndorsements.com endorsements, testimonials, and reviews to your Wordpress web site or blog.
+
+== Description ==
+
+The eEndorsements plugin allows you to easily add your company's eEndorsements.com endorsements, testimonials, and reviews to your Wordpress web site or blog. Rather than having to copy and paste your reviews from various review sites around the web, you can collect them into your eEndorsements.com account and then simply use our plugin shortcode to display your reviews within pages or to create custom widgets.
+
+== Installation ==
+
+1. Go to your admin area and select Plugins -> Add new from the menu.
+2. Search for "eEndorsements".
+3. Click install.
+4. Click activate.
+5. Click settings to configure your API credentials.
